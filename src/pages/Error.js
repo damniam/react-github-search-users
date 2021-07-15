3a53026db2267f63cb11520d6dd3fa3f5832b67e
Damian Dzialo <ddzialo28@interia.pl>
2021-07-15 23:14:33 +0200

    Set Login, Dashboard, Error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -2,13 +2,19 @@ import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 const Error = () => {
-  return <h2>error page</h2>;
+  return <Wrapper>
+    <div>
+      <h1>404</h1>
+      <h3>sorry, the page you are trying to access is not accessible</h3>
+    </div>
+      <Link to="/" className="btn">Back to home</Link>
+    </Wrapper>;
 };
 const Wrapper = styled.section`
   min-height: 100vh;
   display: grid;
   place-items: center;
-  background: var(--clr-primary-10);
+  background: var(--clr-primary-0);
   text-align: center;
   h1 {
     font-size: 10rem;
